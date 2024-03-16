8ba2e6d51d1bd8627e28f7c6f325211042c13e01
Cavin <cavinlarry001@gmail.com>
2024-03-16 03:03:23 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+/* import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ */
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 className='text text-center text-defult'>React App</h1>
 );
-
-// If you want to start measuring performance in your app, pass a function
+ */
+/* // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
+
+const myelement = (
+<h1 className='text text-center text-default'>I Love JSX!</h1>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myelement);
